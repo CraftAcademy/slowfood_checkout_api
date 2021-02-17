877420d34aed6bb6fb4608b60712e47c781e664f
emtalen <59577366+emtalen@users.noreply.github.com>
2021-02-17 11:55:55 +0100

    creating an order with order items and responding with success message, order id and the items in the order

---
+++ app/controllers/api/orders_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Api::OrdersController < ApplicationController
+  def create
+    product = Product.find(params['product_id'])
+    order = current_user.orders.create
+    order.items.create(product: product)
+    if order.persisted?
+      render json: {
+        message: 'This pizza was added to your order!',
+        order_id: order.id,
+        items: order.products
+      }, status: 201
+    else
+      render json: { message: 'Whoops, something went wrong!' }, status: 422
+    end
+  end
+end
